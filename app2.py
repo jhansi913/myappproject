907c7fca54bfad8d1e1952853f3c1616ef87dfb6
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-02-12 13:04:28 +0530

    app2.py

--- app2.py
+++ app2.py
@@ -26,37 +26,12 @@ def model_start():
     st.write(data.head())
     st.text_input("Here are outputs:")
 
-def chatgpt():
-    st.title("ChatGPT-like clone")
-    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-    if "openai_model" not in st.session_state:
-        st.session_state["openai_model"] = "gpt-3.5-turbo"
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    for message in st.session_state.messages:
-         with st.chat_message(message["role"]):
-             st.markdown(message["content"])
-    if prompt := st.chat_input("What is up?"):
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-        with st.chat_message("assistant"):
-            stream = client.chat.completions.create(
-               model=st.session_state["openai_model"],
-               messages=[
-                  {"role": m["role"], "content": m["content"]}
-                  for m in st.session_state.messages
-              ],
-              stream=True,
-        )
-        response = st.write_stream(stream)
-       st.session_state.messages.append({"role": "assistant", "content": response})
-            
+ 
 
         
              
 def main():
-    page = st.sidebar.radio("Navigation", ["Welcome", "prototype","chatbot","model"])
+    page = st.sidebar.radio("Navigation", ["Welcome", "prototype","model"])
 
     if page == "Welcome":
         welcome_page()
@@ -64,8 +39,6 @@ def main():
         login_page()
     elif page == "model":
         model_start()
-    elif page == "chatbot":
-        chatgpt()
 
 if __name__ == "__main__":
     main()
