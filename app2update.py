0e5d7818e3bb218c0779a450a4ebe97b68b6dae0
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-02-15 10:43:03 +0530

    app2update.py

--- app2update.py
+++ app2update.py
@@ -8,26 +8,31 @@ def welcome_page():
     st.title("Welcome to Machine learning world")
     st.subheader("image")
     st.image("machine.jpg")
-def model2():
-    uploaded_file = st.file_uploader("model.pkl")
-    if uploaded_file is not None:
-        clf2 = pickle.loads(uploaded_file.read())
-        st.write("Model loaded")
-        st.write(clf2)
-        st.write("Predicting...")
-        st.write(clf2.predict(X[0:1]))
-        st.write(y[0])
-        st.write("Done!")
+ 
+     
+        
+        
+         
+def make_prediction(model, Temperature,Volume,Pressure,Humidity):
+    input_data = [[Temperature,Volume,Pressure,Humidity]]
+    prediction = model.predict(input_data)
+    return prediction
+
 
 def prototype():
     st.title("PROTOTYPE")
     st.image("prototype.jpg")
 
 def model_start():
-    st.text_input("Enter temp")
-    st.text_input("enter volume")
-    st.text_input("enter pressure")
-    st.text_input("enter humidity")
+    uploaded_file = st.file_uploader("model.pkl")
+    if uploaded_file is not None:
+        clf2 = pickle.loads(uploaded_file.read())
+    Temperature=st.text_input("Enter temp")
+    Volume=st.text_input("enter volume")
+    Pressure=st.text_input("enter pressure")
+    Humidity=st.text_input("enter humidity")
+    power=make_prediction(clf2,Temperature,Volume,Pressure,Humidity)
+    st.write(power)
     df=st.file_uploader('Upload a dataset here')
     data=pd.read_csv('power_test.csv')
     st.line_chart(data) 
