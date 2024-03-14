5ae3a171bbe1debfb2d0df223f8f5d2ab61f28ac
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-03-14 21:22:36 +0530

    logo.py

--- logo.py
+++ logo.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.set_page_config(layout="wide")
-# Custom HTML/CSS for the banner
+# Custom HTML/CSS for the banner"C:\Users\jhans\Desktop\rseume]\logophoto.jpeg"
 custom_html = """
 <div class="banner">
-    <img src="https://www.google.co.in/url?sa=i&url=https%3A%2F%2Fwww.shiksha.com%2Fcollege%2Fgayatri-vidya-parishad-college-of-engineering-for-women-visakhapatnam-46521&psig=AOvVaw2CRCtiJMD7sthv3TAp2yCQ&ust=1710517637640000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCqyvSM9IQDFQAAAAAdAAAAABAE" alt="Banner Image">
+    <img src="logophoto.jpeg" alt="Banner Image">
 </div>
 <style>
     .banner {
