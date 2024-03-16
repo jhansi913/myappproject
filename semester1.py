4b05387787b8aafec49d84473c47701d7dde23f0
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-03-16 20:56:25 +0530

    Add files via upload

---
+++ semester1.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+st.title("GVCEW RESULTS DASHBOARD")
+# read the dataset
+sem_list=['1-1','1-2']
+semester= st.selectbox("Select semester", sem_list, key="selectbox11")
+if semester=='1-1':
+ data = pd.read_excel('22-Res11.xlsx')
+if semester=='1-2':
+ data = pd.read_excel('22-Res12.xlsx')
+ 
+data.dropna(subset=['BRANCH'], inplace=True)
+df=data[data["BRANCH"]==selected_dept]
+df=df.dropna(axis=1, how='all')
+
+dept_list=['CSE','IT','CSM','EEE','ECE']
+selected_dept = st.selectbox("Select Department", dept_list, key="selectbox1")
+
+
+subjects=df.columns[3:11]
+selected_subject = st.selectbox('Select a subject', subjects)
+
+def grades_count():
+        
+        grades_counts = {
+                'O': df[df[selected_subject] == 'O'].shape[0],
+                'A+': df[df[selected_subject] == 'A+'].shape[0],
+                'A': df[df[selected_subject] == 'A'].shape[0],
+                'B+': df[df[selected_subject] == 'B+'].shape[0],
+                'B': df[df[selected_subject] == 'B'].shape[0],
+                'P': df[df[selected_subject] == 'P'].shape[0]
+        }
+        grades_counts_df = pd.DataFrame(list(grades_counts.items()), columns=['Grade', 'Count'])
+        st.write(f"Grades count for {selected_subject} in {selected_department} department:")
+        st.table(grades_counts_df)
+
+
+
+
+def main():
+    st.title("Subject Statistics")
+     
+    # Generate table data
+    table_data = grades_count()
+
+if __name__ == "__main__":
+    main()
+ 
+
+
+
+
+
+
+        
+        
+
+
+ 
