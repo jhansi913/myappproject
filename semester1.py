bc5bae75dd3c34f6f20be8bc254dedf02050c7be
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-03-17 10:30:39 +0530

    semester1.py

--- semester1.py
+++ semester1.py
@@ -24,6 +24,7 @@ subjects=df.columns[3:11]
 selected_subject = st.selectbox('Select a subject', subjects)
 
 def grades_count():
+ 
         
         grades_counts = {
                 'O': df[df[selected_subject] == 'O'].shape[0],
@@ -34,9 +35,23 @@ def grades_count():
                 'C':df[df[selected_subject]=='C'].shape[0],
                 'P': df[df[selected_subject] == 'P'].shape[0]
         }
-        grades_counts_df = pd.DataFrame(list(grades_counts.items()), columns=['Grade', 'Count'])
-        st.write(f"Grades count for {selected_subject} in {selected_dept} department:")
-        st.table(grades_counts_df)
+ grades_counts_df = pd.DataFrame(list(grades_counts.items()), columns=['Grade', 'Count'])
+ st.write(f"Grades count for {selected_subject} in {selected_dept} department:")
+ st.table(grades_counts_df)
+ fig, ax = plt.subplots()
+ plt.pie([tpassed, tfailed, total_strength], labels=['Passed', 'Failed', 'Total Strength'], autopct='%1.1f%%', startangle=90)
+
+# Equal aspect ratio ensures that pie is drawn as a circle.
+ plt.axis('equal')
+  
+
+# Adding a title
+ plt.title('Pass/Fail/Total Strength Pie Chart')
+
+# Showing the plot in Streamlit
+ st.write("Pie Chart:")
+  
+ st.pyplot(fig)
 
 
 
