6b1195fbf8ce526efd2f267da10863461e899259
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-04-05 22:56:19 +0530

    webpage.py

--- webpage.py
+++ webpage.py
@@ -7,15 +7,13 @@ def main():
     with open("home.html", "r", encoding="utf-8") as file1:
         html_content1 = file1.read()
     
-    with open("prediction.html", "r", encoding="utf-8") as file2:
-        html_content2 = file2.read()
+     
 
     # Display HTML content in Streamlit app
     st.subheader("Page 1")
     st.components.v1.html(html_content1, height=600, scrolling=True)
     
-    st.subheader("Page 2")
-    st.components.v1.html(html_content2, height=600, scrolling=True)
+     
 
 if __name__ == "__main__":
     main()
