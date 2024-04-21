45d1dac792d7df2ff33f7a5f0a7ea5c80266d75b
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-04-21 10:36:32 +0530

    jhansi.py

--- jhansi.py
+++ jhansi.py
@@ -18,7 +18,7 @@ dept_list=['CSE','IT','CSM','EEE','ECE']
 selected_dept = st.selectbox("Select Department", dept_list, key="selectbox1")
 df=data[data["BRANCH"]==selected_dept]
 df=df.dropna(axis=1, how='all')
-st.write(df.shape)
+st.write(df.shape[1])
  
  
   
