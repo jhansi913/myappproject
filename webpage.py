24390b4ca3fd70a24ae443307d44eb920dd8de1e
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-05-25 23:51:06 +0530

    webpage.py

--- webpage.py
+++ webpage.py
@@ -2,14 +2,16 @@ import streamlit as st
 
 def main():
     st.title("Embedding HTML in Streamlit")
+    with open('home.html', 'r', encoding='utf-8') as html_file:
+        html_content = html_file.read()
+    st.markdown(html_content, unsafe_allow_html=True)
 
     # Read the HTML files
-   with open('home.html', 'r', encoding='utf-8') as html_file:
-       html_content = html_file.read()
+    
 
      
 # Display the HTML file in Streamlit
-  st.markdown(html_content, unsafe_allow_html=True)
+   
      
 
 if __name__ == "__main__":
