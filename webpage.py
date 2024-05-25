4e21f91758896289b1a78822101dbf7a26804a77
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-05-26 00:18:38 +0530

    webpage.py

--- webpage.py
+++ webpage.py
@@ -1,15 +1,13 @@
 import streamlit as st
 import streamlit.components.v1 as components
-
-# >>> import plotly.express as px
-# >>> fig = px.box(range(10))
-# >>> fig.write_html('test.html')
+ 
 
 st.header("test html import")
 
-HtmlFile = open("home.html", 'r', encoding='utf-8')
+HtmlFile = open("test.html", 'r', encoding='utf-8')
+source_code = HtmlFile.read() 
+ 
 components.html(source_code)
-    
 
      
 # Display the HTML file in Streamlit
