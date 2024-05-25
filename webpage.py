c0183d6d6ada5545f9081d2404e459ca15d09ed1
jhansi913 <132366918+jhansi913@users.noreply.github.com>
2024-05-26 00:17:52 +0530

    webpage.py

--- webpage.py
+++ webpage.py
@@ -8,8 +8,6 @@ import streamlit.components.v1 as components
 st.header("test html import")
 
 HtmlFile = open("home.html", 'r', encoding='utf-8')
-source_code = HtmlFile.read() 
-print(source_code)
 components.html(source_code)
     
 
